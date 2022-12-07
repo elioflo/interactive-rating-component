94a5d3b1867b6be39416f28ab965b22b239493c7
elioflo <eliofloreschoque2357@gmail.com>
2022-12-07 09:34:07 -0300

    Refactor html, css and js files.
    - Use radio button instead of div, and form to send the score.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,10 @@
-let selected = undefined
-
-function selectButton(button) {
-    if(selected){
-        selected.classList.remove('rating__score--selected')
-    }
-    button.classList.add('rating__score--selected')
-    selected = button
-}
-
-Array.from(document.getElementsByClassName('rating__score')).forEach(button => {
-    button.addEventListener('click',()=>{selectButton(button)})
-})
-
-function thanks(){
-    if(selected){
-        document.getElementsByClassName('thanks__score')[0].textContent = `You selected ${selected.textContent} out of 5`
-        document.getElementsByClassName('rating')[0].style.display = "none"
-        document.getElementsByClassName('thanks')[0].style.display = "flex"
-    }
-}
-
-document.getElementsByClassName('rating__button')[0].addEventListener('click',()=>{thanks()})
-
+document
+  .getElementsByClassName('rating__button')[0]
+  .addEventListener('click', (event) => {
+    event.preventDefault()
+    const form = document.getElementsByClassName('rating')[0]
+    const formData = new FormData(form)
+    document.getElementsByClassName('thanks__score')[0].textContent = `You selected ${formData.get('score')} out of 5`
+    form.style.display = 'none'
+    document.getElementsByClassName('thanks')[0].style.display = 'flex'
+  })
