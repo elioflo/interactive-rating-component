ec9f076fe2fc2ac82009935eae3001ff5db93fc8
elioflo <eliofloreschoque2357@gmail.com>
2022-12-08 10:06:00 -0300

    Fix html, css and js
    - Use semantic html tags
    - Use ':has' pseudo-class css selector
    - Use 'submit' form event instead of 'click' button event

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-document
-  .getElementsByClassName('rating__button')[0]
-  .addEventListener('click', (event) => {
-    event.preventDefault()
-    const form = document.getElementsByClassName('rating')[0]
-    const formData = new FormData(form)
-    document.getElementsByClassName('thanks__score')[0].textContent = `You selected ${formData.get('score')} out of 5`
-    form.style.display = 'none'
-    document.getElementsByClassName('thanks')[0].style.display = 'flex'
-  })
+const form = document.getElementById('form')
+const rating = document.getElementsByClassName('rating')[0]
+const thanks = document.getElementsByClassName('thanks')[0]
+const scoreText = thanks.getElementsByClassName('thanks__score')[0]
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const data = new FormData(form)
+  scoreText.textContent = `You selected ${data.get('score')} out of 5`
+  rating.style.display = 'none'
+  thanks.style.display = 'flex'
+})
